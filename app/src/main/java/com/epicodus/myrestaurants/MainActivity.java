19d4592dbd31d39bec081279bb116d857c968e95
Riley Watts <rileywatts.yes@gmail.com>
2017-05-23 11:09:05 -0700

    add arrayAdapter and tests

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -1,23 +1,27 @@
 package com.epicodus.myrestaurants;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
     private static final String TAG = MainActivity.class.getSimpleName();
     private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface seaFont = Typeface.createFromAsset(getAssets(), "fonts/seafont.ttf");
+        mAppNameTextView.setTypeface(seaFont);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
