b53e6ed805fa2c5573200c1502695832e668c764
Riley Watts <rileywatts.yes@gmail.com>
2017-06-01 11:22:13 -0700

    add parcelable

--- app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
+++ app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
@@ -2,20 +2,25 @@ package com.epicodus.myrestaurants.models;
 
 import java.util.ArrayList;
 
+import org.parceler.Parcel;
+
 /**
  * Created by riffee on 5/30/2017.
  */
 
+@Parcel
 public class Restaurant {
-    private String mName;
-    private String mPhone;
-    private String mWebsite;
-    private double mRating;
-    private String mImageUrl;
-    private ArrayList<String> mAddress = new ArrayList<>();
-    private double mLatitude;
-    private double mLongitude;
-    private ArrayList<String> mCategories = new ArrayList<>();
+    String mName;
+    String mPhone;
+    String mWebsite;
+    double mRating;
+    String mImageUrl;
+    ArrayList<String> mAddress = new ArrayList<>();
+    double mLatitude;
+    double mLongitude;
+    ArrayList<String> mCategories = new ArrayList<>();
+
+    public Restaurant() {}
 
     public Restaurant(String name, String phone, String website,
                       double rating, String imageUrl, ArrayList<String> address,
